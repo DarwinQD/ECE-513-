7257d4a7f46ca3f93e98820febc28eb57b70ff70
DarwinQD <61672693+DarwinQD@users.noreply.github.com>
2020-09-01 16:44:55 -0400

    Add files via upload

---
+++ HW1_1.m
@@ -0,0 +1,124 @@
+% Darwin Quiroz
+% 09/01/2020
+% ECE 513
+% Forward Euler method used.
+% NOTE:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Looking back on the assignment, and having better experience doing
+% computations, the constants should have been made dimensionless and the
+% step distance (dt) should have been a function of Nsteps. along with just
+% setting the constants such as charge, epsilon, etc.. to 1 so to not
+% influence the graph. and have used the Runga Kutta method over the
+% forward Euler. This way would have allowed the E field equation to not be
+% influenced by small distances (as a ratio) compared to what was
+% developed. These methods mentioned were used when creating and
+% implementing question 2 for continuous charge distributions.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% number of samples that will be taken
+Nsteps = 10000;
+% differential time/distance taken
+dt = 0.01;
+% initialization of arrays (used for other variables such as E1 and E2 so
+% to analyze data computer during execution
+x = zeros(1,Nsteps);
+y = zeros(1,Nsteps);
+t = zeros(1,Nsteps);
+E_x1 = zeros(1,Nsteps-1);
+E_y1 = zeros(1,Nsteps-1);
+E_x2 = zeros(1,Nsteps-1);
+E_y2 = zeros(1,Nsteps-1);
+%position of charges
+x_charge1 = -3;
+y_charge1 = 0;
+x_charge2 = 3;
+y_charge2 = 0;
+%setting constants used for E-field formula
+epsilon = 8.854*10^(-12);
+k = 1/(4*pi*epsilon);
+% charge values are given
+q1 = -1*10^(-9); % charges will be set 1 nC for simplicity
+q2 = 1*10^(-9);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2 for loops will be created, each loop will calculate a field line and
+% store these values into an array to plot
+% splitting positive and negative x values into seperate arrays
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% initial condition for x created (will be moved within the for loop to
+% plot field lines to the left of the negative charge
+x(1) = x_charge2 + 0.1;
+t(1) = 0;
+r1 = zeros(1,Nsteps);
+r2 = zeros(1,Nsteps);
+%number of field lines to be drawn in total (secondary for loop will use)
+Nlines = 20;
+fprintf('t\tx\ty\n')
+% want N field lines for each charge to be drawn
+% outer for loop to draw a new field line from charge at a different angle 
+for i_2 = 1:(Nlines)
+    % a total of N field lines are going to be drawn N/2 for each charge
+    % first half will start using location of positive charge
+    if i_2 <=(Nlines/2)  
+        theta = (2*pi/Nlines)*2*i_2;
+        x(1) = x_charge2 + cos(theta);
+        y(1) = y_charge2 + sin(theta);
+    % second half of loop will use location of negative charge
+    else
+        % theta starts at 0 from the +x-axis and increments by an evenly
+        theta = (2*pi/Nlines)*2*i_2;
+        x(1) = x_charge1 + cos(theta);
+        y(1) = y_charge1 + sin(theta);
+    end
+    for i = 1:(Nsteps-1)
+        fprintf('%.3f\t%.3f\t%.3f\n',t(i),x(i),y(i));
+        % calculates the distance each loop between charges and current
+        % position
+        r1(i) = (((x(i) - x_charge1)^2 + (y(i) - y_charge1)^2)^(3/2));
+        r2(i) = (((x(i) - x_charge2)^2 + (y(i) - y_charge2)^2)^(3/2));
+        % E field is then calculated using Coulombs law for both charges
+        E_x1(i) = k*q1*(x(i) - x_charge1)/r1(i);
+        E_y1(i) = k*q1*(y(i) - y_charge1)/r1(i);
+     
+        E_x2(i) = k*q2*(x(i) - x_charge2)/r2(i);
+        E_y2(i) = k*q2*(y(i) - y_charge2)/r2(i);
+        % using Forward Euler method, system becomes unstable when
+        % approaching the negative charge (distance between point and
+        % charge becomes smaller as one approaches the positive charge and
+        % E field becomes extremely large fluctuation position
+        if (x(i) <= x_charge1*0.98)
+            % just causes the default valuet to stay at the charge one near
+            x(i+1) = x_charge1;
+            y(i+1) = y_charge1;
+            t(i+1) = t(i) + dt*t(i);
+        else
+            % otherwise continues calculation the new position due to the
+            % influence of the fields
+            x(i+1) = x(i) + dt*(E_x1(i) + E_x2(i));
+            y(i+1) = y(i) + dt*(E_y1(i) + E_y2(i));
+            t(i+1) = t(i) + dt;
+        end
+    end  
+% once the data for different positional points have been calculated due to
+% the E-fields influence, the line is plotted onto the graph for eahc line
+plot(x,y);
+% hold on so to plot all field lines onto the same graph
+hold on;
+ax = gca;
+% set axis so to show with reference to the origin
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+set(0,'DefaultLegendAutoUpdate','off')
+axis square
+xlabel('x')
+ylabel('y')
+title('point charges field lines');
+print -dpdf HW_11.pdf
+end
+
+
+
+
+
+
+
---
+++ HW1_2.m
@@ -0,0 +1,70 @@
+index = 1;
+% N must be creater than 2 for the seperation of charges to be less than
+% the length of the line
+N = 3;
+% Ey_ratio will be the output and set to any value bigger than 0.01 to 
+% start while loop
+
+% Ey_ratio = abs(1 - Ey_approximate/Ey_exact)
+Ey_ratio = 1;
+output = ones(1,51);
+delta = zeros(1,50);
+% While loop will keep checking each N increating each loop to determine
+% when the difference between the approximation and exact electric field is
+% less than 1%
+while(Ey_ratio > 0.01)
+
+%The new limits for the summation are adjusted since N is now increased to
+%a higher value (alternating between odd and even)
+N_even_limit = N/2 - 1;
+N_odd_limit = (N-1)/2;
+% resets to the sum to 0 for each iteration of N
+sum = 0;
+% odd_even determines if N is either odd or even 
+odd_even = rem(N,2);
+% 2 cases are made to determine which summation to use 
+% odd case 
+if (odd_even == 1)
+   for i = 1:N_odd_limit
+       sum = sum + 2/( (1 + (2*i/(N-1))^2)^(3/2) );
+   end
+   sum = sum + 1;
+% even case
+else
+   for i = 0:N_even_limit
+        sum = sum + 2/( ( ((1 + 2*i) / (N-1))^2 +1)^(3/2) );
+   end   
+end
+% depending if N is even or odd, Ey_ratio uses the summation calculated 
+% for its appropriate N case  
+Ey_ratio = 1-(sqrt(2)/N) * sum;
+% take the ratio outputs and record them into an array to show the increase
+% in approximation
+output(N) = ((sqrt(2)/N) * sum);
+delta(N) = 2/(N-1);
+index = index + 1;
+% N is increased by 1 for the next loop iteration
+N = N + 1;
+end
+
+% for data, output(1) and output(2) were calculated using the ratio and
+% comparing for N = 1,2
+
+output(1) = NaN;
+output(2) = NaN;
+delta(1) = NaN;
+delta(2) = NaN;
+
+N_charges = 1:51;
+subplot(2,1,1)
+plot(N_charges, delta)
+xlabel('Charges on Line')
+ylabel('delta (units of 1/L)')
+title('Delta output correlation to number of charges');
+subplot(2,1,2)
+hold
+plot(N_charges, output)
+xlabel('# of Charges')
+ylabel('E field ratio $\frac{Approximate}{Exact}$','Interpreter','Latex')
+title('approximation accuracy');
+print -dpdf HW_12.pdf
